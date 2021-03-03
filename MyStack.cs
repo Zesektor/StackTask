18bd90f02d0fe0983d2bbba69d8a12fbd8f43bf2
Alexey Tkachuk <sektor_les@mail.ru>
2021-03-03 16:18:14 +0300

    Add project files.

---
+++ MyStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackTask
+{
+    public class MyStack<T> : IStack<T>
+    {
+        private int _i;
+
+        private T[] array;
+
+        public MyStack()
+        {
+            _i = -1;
+            array = new T[1000];
+        }
+
+        public void Push(T x)
+        {
+            _i++;
+            array[_i] = x;
+        }
+
+        public T Pop()
+        {
+            if (_i == -1) return default(T);
+            var result = array[_i];
+            _i--;
+            return result;
+        }
+
+        public bool IsEmpty()
+        {
+            return _i == -1;
+        }
+
+        public MyStack<T> Reverse()
+        {
+            var newStack = new MyStack<T>();
+            for (int i = _i; i >= 0; i--)
+            {
+                newStack.Push(array[i]);
+            }
+
+            return newStack;
+        }
+    }
+
+    public interface IStack<T>
+    {
+        void Push(T x);
+        T Pop();
+        bool IsEmpty();
+        MyStack<T> Reverse();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StackTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new MyStack<string>();
+            s.Push("a");
+            s.Push("b");
+            s.Push("c");
+            s.Push("d");
+            s.Push("e");
+
+            var el = s.Pop();
+            Console.WriteLine(el);
+
+            el = s.Pop();
+            Console.WriteLine(el);
+
+            Console.WriteLine("----------------------");
+
+            var newStack = s.Reverse();
+            while (!newStack.IsEmpty())
+            {
+                var newEl = newStack.Pop();
+                Console.WriteLine(newEl);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
